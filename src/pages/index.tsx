ffb75effd45d9d17d5c54a9b2236dd243c5ad3eb
elshanx <elshanxakbarov@gmail.com>
2021-11-09 03:38:31 +0400

    clean up home page and comment what's causing the error in GSSP

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,23 @@
 import { GetServerSideProps, NextPage } from 'next'
-import { getPokemon, getRunningOperationPromises } from 'src/services/pokemon'
+import { getPokemon, getRunningOperationPromises, useGetPokemonQuery } from 'src/services/pokemon'
 import { wrapper } from 'src/store'
 
 const Home: NextPage = () => {
-  return <span>test</span>
+  const result = useGetPokemonQuery(null)
+
+  return (
+    <>
+      <pre>
+        <code>{JSON.stringify(result, null, 2)}</code>
+      </pre>
+    </>
+  )
 }
 
 export const getServerSideProps: GetServerSideProps = wrapper.getServerSideProps(
   store => async () => {
-    store.dispatch(getPokemon.initiate())
+    // * store.dispatch(getPokemon.initiate()) this causes an error
+    store.dispatch(getPokemon.initiate(null)) // this doesn't
 
     await Promise.all(getRunningOperationPromises())
 
